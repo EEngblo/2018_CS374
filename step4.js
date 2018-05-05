8c2e3ed0996754ab66579482bc8dd2a2b773368a
Jinyoung Oh <sheogorath0213@kaist.ac.kr>
2018-05-05 16:43:06 +0900

    Step 4 case 외형 제작 완료

---
+++ step4.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+
+  $('.special.cards .image').dimmer({
+    on: 'hover'
+  });
+
+
+  $(".rating").rating('disable');
+
+  $( ".controlgroup1" ).controlgroup();
+
+  $( ".controlgroup2" ).controlgroup()
+
+  $('.asdf.checkbox').checkbox();
+});
