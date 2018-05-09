cdab0322c65d7e7dae24f922c3f1fb0e900f3fcd
Jehyun <sammy25@naver.com>
2018-05-09 17:40:32 +0900

    step3.js 추가

---
+++ step3.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+
+  $('.special.cards .image').dimmer({
+    on: 'hover'
+  });
+
+
+  $(".rating").rating('disable');
+
+  $( ".sideFilter" ).controlgroup();
+
+  $( ".colorFilter" ).controlgroup()
+
+});
