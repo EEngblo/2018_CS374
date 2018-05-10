0368f6b909b42277ba22a812532f5bc43151cd59
Jinyoung Oh <sheogorath0213@kaist.ac.kr>
2018-05-11 04:54:06 +0900

    outerFrame으로 모든 컨텐츠 통합 후 컨텐츠 사이 이동 구현 완료
    상단바 인터페이스 대폭 수정

---
+++ main.js
@@ -0,0 +1,54 @@
+
+var steps = ['Home', 'GPUandCPU', 'RAMandSto', 'Case', 'Final']; // mode(게임성능/활용도)에 따라서 step 결정
+var steps_finished = [true, false, false, false, false]; // steps의 각 step이 완료되었는지 저장
+var currentStepIdx = 0; // steps 배열에서 몇 번째 step을 진행중인지 표시
+
+
+
+
+$(document).ready(function(){
+
+
+
+});
+
+//////////////////////////////////////////////////
+
+function moveStep(target){
+  // target으로 들어온 단계로 넘어갈 수 있을 때, 그 단계로 넘어가는 함수
+
+  // breadcrumb update
+  $('#b_GPUandCPU_button').removeClass('active');
+  $('#b_RAMandSto_button').removeClass('active');
+  $('#b_Case_button').removeClass('active');
+  $('#b_Final_button').removeClass('active');
+
+  $('#b_' + target + '_button').addClass('active').blur();
+
+  // main content update
+  $('#d_root_GPUandCPU').attr('hidden', true);
+  $('#d_root_RAMandSto').attr('hidden', true);
+  $('#d_root_Case').attr('hidden', true);
+  $('#d_root_Final').attr('hidden', true);
+
+  $('#d_root_' + target).attr('hidden', false);
+
+  // update currentStepIdx
+  currentStepIdx = steps.indexOf(target);
+  //////console.log(currentStepIdx);
+}
+
+function moveStepButton(isNext){
+  // step 이동 버튼을 클릭했을 때 적절하게 이동하는 함수
+
+  if(isNext){
+    currentStepIdx++;
+  }else{
+    currentStepIdx--;
+  }
+
+  moveStep(steps[currentStepIdx]);
+}
+
+
+////////////////////////////////////////////////////////////////////
