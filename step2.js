f1aad40d7562df5166b6aadad4c598e4d3b351b6
Reminiel <reminiel@naver.com>
2018-05-10 14:07:10 +0900

    add active graph

--- step2.js
+++ step2.js
@@ -3,8 +3,8 @@ var dr_gpuDataTable;
 var dr_gpuTable;
 var dr_cpuTable;
 var dr_readyCallback = function(data){
-    console.log("callback");
-    dr_gpuTable = $('#dr_gpuDataTables');
+  dr_gpuTable = $('#dr_gpuDataTables');
+  dr_cpuTable = $('#dr_cpuDataTables');
     dr_gpuDataTable = dr_gpuTable.DataTable({
         "scrollY" : "520px",
         "scrollCollapse": true,
@@ -12,27 +12,33 @@ var dr_readyCallback = function(data){
         "searching": false,
         "bInfo" : false,
         "order": [[2, 'desc']],
+      "createdRow": function( row, data, dataIndex ){
+        $(row).css("background-size", (50 + data[3]*0.02) + "px 50px", "");
+      },
         "columns": [
-            {"width": "50px", "orderable": false},
-            {"width": "180px", "orderable": false},
-            {"width": "80px"},
-            {"width": "80px"},
-            {"width": "80px"}
+            {"width": "50px", "orderable": false, "className": "dr_gpu_1"},
+            {"width": "180px", "orderable": false, "className": "dr_gpu_2"},
+            {"width": "80px", "className": "dr_gpu_3"},
+            {"width": "80px", "className": "dr_gpu_4"},
+            {"width": "80px", "className": "dr_gpu_5"}
         ]
     });
-    dr_cpuDataTable = $('#dr_cpuDataTables').DataTable({
+    dr_cpuDataTable = dr_cpuTable.DataTable({
         "scrollY" : "520px",
         "scrollCollapse": true,
         "paging": false,
         "searching": false,
         "bInfo": false,
         "order": [[2, 'desc']],
+      "createdRow": function( row, data, dataIndex ){
+        $(row).css("background-size", (50 + data[3]*5) + "px 50px", "");
+      },
         "columns": [
-            {"width": "50px", "orderable": false},
-            {"width": "180px", "orderable": false},
-            {"width": "80px"},
-            {"width": "80px"},
-            {"width": "80px"}
+            {"width": "50px", "orderable": false, "className": "dr_cpu_1"},
+            {"width": "180px", "orderable": false, "className": "dr_cpu_2"},
+            {"width": "80px", "className": "dr_cpu_3"},
+            {"width": "80px", "className": "dr_cpu_4"},
+            {"width": "80px", "className": "dr_cpu_5"}
         ]
     });
 
@@ -40,10 +46,14 @@ var dr_readyCallback = function(data){
         dr_cpuDataTable.row.add(db_cpu[i]).draw(false);
         dr_gpuDataTable.row.add(db_gpu[i]).draw(false);
     }
-    dr_gpuTable.find('tbody').on('click', 'tr', function(){
-        var data = dr_gpuDataTable.row(this).data();
-        console.log(data);
-    });
+  dr_gpuTable.find('tbody').on('click', 'tr', function(){
+    var data = dr_gpuDataTable.row(this).data();
+    console.log(data);
+  });
+  dr_cpuTable.find('tbody').on('click', 'tr', function(){
+    var data = dr_cpuDataTable.row(this).data();
+    console.log(data);
+  });
 };
 
 $(document).ready(function(){
