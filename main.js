746bc5fb25a272ef639b915512017e1a83a01947
Jinyoung Oh <sheogorath0213@kaist.ac.kr>
2018-05-13 16:49:28 +0900

    Revert "Revert "db수정및초기선택추가""
    This reverts commit ad65819503f96f24f8832f48039824ccf1475625.

--- main.js
+++ main.js
@@ -160,10 +160,11 @@ function Composition(){
   if(this.RAM) $("#d_RAM").attr('hidden', true);
 
   //console.log('Success!!');
-
-
+  this.showCPUIdx =0;
+  this.showGPUIdx =0;
+  this.GPU =6;
+  this.CPU =5;
   this.updatePrice(this.price);
-  dr_readyCallback(this.showGPUIdx, this.showCPUIdx);
 
 }
 
@@ -382,6 +383,8 @@ window.onload = function(){
   $('#m_root_Loader').attr('hidden', true);
   $('#m.m_outerScreen').attr('hidden', false);
   $('.stateIndicator').attr('hidden',false);
+  dr_readyCallback();
+
 }
 
 
